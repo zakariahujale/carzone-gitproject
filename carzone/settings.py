2f52401b761603214105024e6b7069721d6cd433
Zakaria Hujale <sakeriyehujale@gmail.com>
2021-02-05 04:28:14 +0300

    working css and logo

--- carzone/settings.py
+++ carzone/settings.py
@@ -120,6 +120,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-staticfiles_DIRS = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'carzone/static'),
 ]
